ffd0b62b8d226b9eb7bbb98f43283609d559b8e7
halimyusuf <haleemyoosuph@gmail.com>
2021-09-27 17:53:07 +0100

    feat: add nav

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,28 @@
+import Image from "next/image";
+import styles from "../styles/navbar.module.css";
+
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.nav_inner}>
+        <div className={styles.left_nav}>
+          <Image
+            src="/my_unsplash_logo.svg"
+            alt="..."
+            width="150"
+            height="40"
+          />
+          <div>
+            <input type="text" placeholder="Search by name" />
+          </div>
+        </div>
+        <div>
+          <div className={styles.nav_btn}>
+            {" "}
+            <div>Add a photo</div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
