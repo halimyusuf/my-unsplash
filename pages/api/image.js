71e66be3a84aaa75e1656389e6e3de68ef046949
halimyusuf <haleemyoosuph@gmail.com>
2021-10-02 18:03:07 +0100

    feat: save and get images

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,23 +1,52 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import cloudinary from "cloudinary";
+import nc from "next-connect";
 import intializeDB from "../../config/mongoose";
 import ENVS from "../../config/process.env";
 import logger from "../../utils/logger";
+import Image from "../../model/Image";
+
+const app = nc();
 
 intializeDB();
 cloudinary.config(ENVS.cloudinaryUrl);
 
-export default function handler(req, res) {
-  if (req.method == "POST") {
-    logger(req.body);
-    // cloudinary.v2.uploader.upload(
-    //   "https://images.unsplash.com/photo-1606787947360-4181fe0ab58c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDF8MXxhbGx8MjZ8fHx8fHwyfHwxNjMyOTI0MDQ2&ixlib=rb-1.2.1&q=80&w=1080",
-    //   function (err, result) {
-    //     console.log(result);
-    //   }
-    // );
-    res.status(200).json({ name: "John Doe" });
-  } else {
-    res.status(404).json({ name: "Invalid request" });
-  }
+function cloudinaryHelp(url) {
+  console.log(url);
+  return new Promise((resolve, reject) => {
+    cloudinary.v2.uploader.upload(url, function (err, result) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.secure_url);
+      }
+    });
+  });
 }
+
+app.post(async (req, res) => {
+  const { label } = req.body;
+  try {
+    const url = await cloudinaryHelp(req.body.imageUrl);
+    let newImg = new Image({ url, label });
+    try {
+      newImg = await newImg.save();
+      res.status(200).json({ data: newImg });
+    } catch (error) {
+      logger(error);
+      res.status(500).json({ error: "Server error" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "Invalid image url" });
+  }
+});
+
+app.get(async (req, res) => {
+  const images = await Image.find();
+  // console.log(JSON.stringify(images));
+  return res.status(200).json({ data: images });
+});
+
+// await Kitten.find({ name: /^fluff/ });
+export default app;
