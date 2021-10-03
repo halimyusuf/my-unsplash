785655e3bbc4652020ccd5627ab7c3321f9e2758
halimyusuf <haleemyoosuph@gmail.com>
2021-10-03 01:40:57 +0100

    style: add support for more viewport

--- components/RenderImages.js
+++ components/RenderImages.js
@@ -7,8 +7,8 @@ export default function RenderImages({ images }) {
         {images.map((a) => (
           <div
             key={a.url}
-            className="col-md-4 px-2 my-2"
-            style={{ maxWidth: "100%", margin: "auto" }}
+            className="col-sm-6 col-md-4 px-2 my-2"
+            style={{ maxWidth: "100%", margin: "" }}
           >
             <div className="rend-image-cont">
               <div className="img-modal">
@@ -17,7 +17,7 @@ export default function RenderImages({ images }) {
                   <div className="img-modal-text">{a.label}</div>
                 </div>
               </div>
-              <Image src={a.url} width="500" height="300" alt={a.label} />
+              <Image src={a.url} width="500" height="350" alt={a.label} />
             </div>
           </div>
         ))}
